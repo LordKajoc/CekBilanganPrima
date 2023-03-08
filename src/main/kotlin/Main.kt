c3ae1d1dfb750a728465ade1273f31ae5d2ef347
LordKajoc <rizkinuriman@upi.edu>
2023-03-08 20:05:17 +0700

    Commit 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,4 @@
 fun main() {
-    cekbilanganprima()
-}
-
-fun cekbilanganprima() {
-    var bilanganprima :Boolean =true
-    print("Masukan sebuah bilangan bulat positif: ")
-    var inputangka: Int = readln().toInt()
-    if (inputangka ==0 ||inputangka == 1) {
-        bilanganprima = false
-    } else{
-        for (i in 2..inputangka/2){
-            if (inputangka % i == 0){
-                bilanganprima = false;
-                break
-            }
-        }
+    val jalankan = kondisi()
+    jalankan.cekbilanganprima()
     }
-
-    if (bilanganprima){
-        println("$inputangka Merupakan Bilangan Prima")
-    }else{
-        println("$inputangka Bukan Merupakan Bilangan Prima")
-    }
-}
